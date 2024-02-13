d956e905bb8edb3df54d0273152ff2581e073cdf
nthskyradiated <andy.pandaan@outlook.com>
2024-02-13 22:11:40 +0400

    added animations

--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -4,6 +4,15 @@ const menu = document.getElementById('mobile-menu');
 
 btn.addEventListener('click', navToggle);
 
+//note: this function is for Astro's view transition
+document.addEventListener('astro:after-swap', () => {
+  btn.classList.remove('open');
+  overlay.classList.remove('overlay-show');
+  document.body.classList.remove('stop-scrolling');
+  menu.classList.remove('show-menu');
+})
+
+
 function navToggle() {
   btn.classList.toggle('open');
   overlay.classList.toggle('overlay-show');
